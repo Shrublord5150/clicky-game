83075ab3a3bcee234c4254e3f94f7391504e5ae1
Daniel Murphy <tugs.murphy@yahoo.com>
2019-01-18 22:10:10 -0500

    Updates

---
+++ precache-manifest.c5893e8bb1020db5c0cfe87f997a2fc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "9d64df777af383875bf5",
+    "url": "/clicky-game/static/js/main.9d64df77.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/clicky-game/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "9d64df777af383875bf5",
+    "url": "/clicky-game/static/css/main.6c156456.chunk.css"
+  },
+  {
+    "revision": "d0beebcfa6f959a743594510e2edbcfb",
+    "url": "/clicky-game/index.html"
+  }
+];
